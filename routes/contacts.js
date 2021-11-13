8de384ab1863629403e8a62b136798c47e7eaf2c
Daniel Sângelo <dpassos@nelogica.com.br>
2021-11-13 15:25:20 -0300

    creating and adding authenticate middleware to routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
 	const { contacts } = app.controllers;
-	app.get('/contacts', contacts.index);
-	app.get('/contacts/:id', contacts.show);
-	app.post('/contacts', contacts.create);
-	app.get('/contacts/:id/edit', contacts.edit);
-	app.put('/contacts/:id', contacts.update);
-	app.delete('/contacts/:id', contacts.destroy);
+	app.get('/contacts', authenticate, contacts.index);
+	app.get('/contacts/:id', authenticate, contacts.show);
+	app.post('/contacts', authenticate, contacts.create);
+	app.get('/contacts/:id/edit', authenticate, contacts.edit);
+	app.put('/contacts/:id', authenticate, contacts.update);
+	app.delete('/contacts/:id', authenticate, contacts.destroy);
 }
