212517f318c91e4b19442a20cc5b244f60b22b92
Daniel Sângelo <dpassos@nelogica.com.br>
2021-11-15 14:51:05 -0300

    Implementing status notification

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,7 +1,16 @@
+const crypto = require('crypto');
+
 module.exports = (app) => {
 	const ChatController = {
 		index (req, res){
-			res.render('chat/index');
+			const { room } = req.query;
+			let roomHash = room;
+			if (!roomHash){
+				const timestamp = Date.now().toString();
+				const md5 = crypto.createHash('md5');
+				roomHash = md5.update(timestamp).digest('hex');
+			}
+			res.render('chat/index', {room: roomHash });
 		}
 	}
 	return ChatController;
--- events/chat.js
+++ events/chat.js
@@ -1,11 +1,36 @@
 module.exports = (app, io) => {
+	const onlines = {};
 	io.on('connection', (client) => {
 		const { session } = client.handshake;
 		const { user } = session;
-		client.on('send-server', (msg) => {
+		onlines[user.email] = user.email;
+
+		for(let email in onlines){
+			client.emit('nofity-onlines', email);
+			client.broadcast.emit('notify-onlines', email);
+		}
+
+		client.on('send-server', (hashRoom, msg) => {
+			const newMessage = { email: user.name, room: hashRoom };
 			const response = `<b>${user.name}:</b> ${msg}<br>`;
-			client.emit('send-client', response);
-			client.broadcast.emit('send-client', response);
+			session.room = hashRoom;
+			client.broadcast.emit('new-message', newMessage);
+			io.to(hashRoom).emit('send-client', response);
 		});
+
+		client.on('create-room', (hashRoom) => {
+			session.room = hashRoom;
+			client.join(hashRoom);
+		});
+
+		client.on('disconnect', () => {
+			const { room } = session;
+			const response = `<b>${user.name}:</b> saiu <br>`;
+			delete onlines[user.email];
+			session.room = null;
+			client.leave(room);
+			client.broadcast.emit('notify-offlines', user.email);
+			io.to(room).emit('send-client', response);
+		})
 	});
 };
